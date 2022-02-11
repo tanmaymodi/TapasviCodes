e380db43bbe9250e5328cf3a23dc3b00d7a93fec
Tanmay Modi <tanmaymodi24@gmail.com>
2022-02-11 20:43:17 +0530

    db of all colllections created

--- models/user.js
+++ models/user.js
@@ -1,16 +1,112 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-
-const FormSchema = new Schema({
+//patient details schema
+const patientDetails = new Schema({
     name: {
         type: String,
-        required: [true, "Formname is required"]
+        required: [true, "Nmae of patient is required"]
     }, 
-    fields: {
-        type: Object,
-        required: [true, "min no. of fields = 1"]
+    age: {
+        type: Number,
+        required: [ture, "Age of patient is required"]
+    },
+    gender: {
+        type: String,
+        required: [ture, "Gender of patient is required"]
+    },
+    email: {
+        type: Email,
+        required: [ture, "Email of patient is required"]
+    },
+    password: {
+        type: password,
+        required: [ture, "Password of patient is required"]
+    },
+    diagnosis: {
+        type: String,
+        required: [false, "Symptomps and diagnostics of the patient"]
     }
 })
 
-const Form = mongoose.model("Form", FormSchema);
-module.exports = Form;
+//Hostial Staff Details
+const staffDetails = new Schema({
+    name: {
+        type: String,
+        required: [true, "Staff Member name is required"]
+    },
+    email: {
+        type: Email,
+        required: [true, "Staff Member email is required"]
+    },
+    hospitalRegistrationNumber: {
+        type: String,
+        required: [true, "Staff Member's working hospital number is required"]
+    },
+    password: {
+        type: password,
+        required: [true, "Staff Member password is required"]
+    }
+})
+
+//Hospital Details Schema
+const hospitalDetails = new Schema({
+    name: {
+        type: String,
+        required: [true,"Name of the hospital is required"]
+    },
+    facilities: {
+        type: String,
+        required: [true,"Facilities of the hospital is required"]
+    },
+    address: {
+        type: String,
+        required: [true,"Address of the hospital is required"]
+    },
+    numberOfBeds: {
+        type: Number,
+        required: [true,"Number of the hospital is required"]
+    },
+    hospitalRegistrationNumber: {
+        type: String,
+        required: [ture,"Hospital Registration Number is required"]
+    }
+})
+
+//patient transfer detials
+const patientTransferDetails = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name of transfering patient is required"]
+    },
+    emailOfPatient: {
+        type: Email,
+        required: [true, "Email of transfering patient is required"]
+    },
+    fromId: {
+        type: String,
+        required: [true, "From hospital id of transfering patient is required"]
+    },
+    toId: {
+        type: String,
+        required: [true, "To hospital id of transfering patient is required"]
+    },
+    date: {
+        type: Date,
+        required: [true, "Date of transfering patient is required"]
+    },
+    diagnosis: {
+        type: String,
+        required: [true, "Tests done so far of transfering patient is required"]
+    },
+
+})
+
+
+const patientDetails = mongoose.model("patientDetails", patientDetails);
+const staffDetails = mongoose.model("staffDetails", staffDetails);
+const hospitalDetails = mongoose.model("hospitalDetails", hospitalDetails);
+const patientTransferDetails = mongoose.model("patientTransferDetails", patientTransferDetails);
+module.exports = patientDetails;
+module.exports = staffDetails;
+module.exports = hospitalDetails;
+module.exports = patientTransferDetails;
